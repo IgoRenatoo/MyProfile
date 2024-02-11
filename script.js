9414dfc0debf8f503ec3d89ffa478ee0340da76f
IgoRenatoo <igor.renato.oliveira@gmail.com>
2024-02-11 12:15:26 -0300

    Att Switch DarkMode

--- script.js
+++ script.js
@@ -1,61 +1,75 @@
-function toggleMode(){
-  const html = document.documentElement
-  html.classList.toggle("light")
-}   
+//Start DarkMode
+function toggleMode() {
+  const html = document.body;
+  html.classList.toggle("light");
 
-function load(){
-  
-  var hra1 = document.getElementById('hra')
-  var data = new Date()
-  var hrg = data.getHours()
-  var hra11 = Number(hrg)
-  var at = document.getElementById('tempo')
-  var af = document.getElementById('foto')
-  hra1.innerHTML =('Agora são ' + hra11 + ' horas!')
+  var mode;
 
-
-  if (hra11 >= 0 && hra11 <= 12) {
-    at.innerHTML = ('Olá, muito bom dia!')
-    af.src = "./css/assets/data/dia.png"
-
-  }
-  if (hra11 > 12 && hra11 < 18){
-    at.innerHTML = ('Olá, muito boa tarde!')
-    af.src = "./css/assets/data/tarde.png"
-  }
-  if (hra11 >= 18 && hra11 < 23.59){
-    at.innerHTML = ('Olá, muito boa noite!')
-    af.src = "./css/assets/data/noite.png"
+  if (html.classList.contains("light")) {
+    mode = "light";
+  } else {
+    mode = "dark";
   }
 
+  localStorage.setItem("PageTheme", JSON.stringify(mode));
 }
 
-function time(){
-  var hr1 = document.getElementById('hr')
-  var hr2 = Number(hr1.value)
-  var tm = document.getElementById('tempo')
-  var ft = document.getElementById('foto')
-  var hras1 = document.getElementById('hra')
+const getMode = JSON.parse(localStorage.getItem("PageTheme"));
+console.log(getMode);
 
+if (getMode === "light") {
+  document.body.classList = "light";
+}
+//End DarkMode
+
+function load() {
+  var hra1 = document.getElementById("hra");
+  var data = new Date();
+  var hrg = data.getHours();
+  var hra11 = Number(hrg);
+  var at = document.getElementById("tempo");
+  var af = document.getElementById("foto");
+  hra1.innerHTML = "Agora são " + hra11 + " horas!";
+
+  if (hra11 >= 0 && hra11 <= 12) {
+    at.innerHTML = "Olá, muito bom dia!";
+    af.src = "./css/assets/data/dia.png";
+  }
+  if (hra11 > 12 && hra11 < 18) {
+    at.innerHTML = "Olá, muito boa tarde!";
+    af.src = "./css/assets/data/tarde.png";
+  }
+  if (hra11 >= 18 && hra11 < 23.59) {
+    at.innerHTML = "Olá, muito boa noite!";
+    af.src = "./css/assets/data/noite.png";
+  }
+}
+
+function time() {
+  var hr1 = document.getElementById("hr");
+  var hr2 = Number(hr1.value);
+  var tm = document.getElementById("tempo");
+  var ft = document.getElementById("foto");
+  var hras1 = document.getElementById("hra");
 
   if (hr2 >= 0 && hr2 <= 12) {
-    tm.innerHTML = ('Olá, muito bom dia!')
-    ft.src = "./css/assets/data/dia.png"
-    hras1.innerHTML = ('Agora são ' + hr2 + ' horas!')
+    tm.innerHTML = "Olá, muito bom dia!";
+    ft.src = "./css/assets/data/dia.png";
+    hras1.innerHTML = "Agora são " + hr2 + " horas!";
   }
 
-  if (hr2 > 12 && hr2 < 18){
-    tm.innerHTML = ('Olá, muito boa tarde!')
-    ft.src = "./css/assets/data/tarde.png"
-    hras1.innerHTML = ('Agora são ' + hr2 + ' horas!')
-  } 
-
-  if (hr2 >= 18 && hr2 < 23.59){
-    tm.innerHTML = ('Olá, muito boa noite!')
-    ft.src = "./css/assets/data/noite.png"
-    hras1.innerHTML = ('Agora são ' + hr2 + ' horas!')
+  if (hr2 > 12 && hr2 < 18) {
+    tm.innerHTML = "Olá, muito boa tarde!";
+    ft.src = "./css/assets/data/tarde.png";
+    hras1.innerHTML = "Agora são " + hr2 + " horas!";
   }
-  if (hr2 > 24 || hr2 == null ) {
-      hras1.innerHTML = 'Esse horário não existe!'
+
+  if (hr2 >= 18 && hr2 < 23.59) {
+    tm.innerHTML = "Olá, muito boa noite!";
+    ft.src = "./css/assets/data/noite.png";
+    hras1.innerHTML = "Agora são " + hr2 + " horas!";
+  }
+  if (hr2 > 24 || hr2 == null) {
+    hras1.innerHTML = "Esse horário não existe!";
   }
 }
