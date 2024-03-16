75491a0cc4a9036cc08561ffd56991748f1283fa
IgoRenatoo <igor.renato.oliveira@gmail.com>
2024-03-16 20:24:43 -0300

    NewInitial Commit

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,6 @@
 //Start DarkMode -------------------------------------------------
 const getMode = JSON.parse(localStorage.getItem("PageTheme"));
-console.log(getMode);
+//console.log(getMode);
 if (getMode === "light") {
   document.body.classList = "light";
 }
@@ -9,142 +9,10 @@ function toggleMode() {
   html.classList.toggle("light");
 
   var mode;
-
   if (html.classList.contains("light")) {
     mode = "light";
   } else {
     mode = "dark";
   }
-
   localStorage.setItem("PageTheme", JSON.stringify(mode));
 }
-// Start Projetos -------------------------------------------------
-function timeAuto() {
-  var hra1 = document.getElementById("hra");
-  var data = new Date();
-  var hrg = data.getHours();
-  var hra11 = Number(hrg);
-  var at = document.getElementById("tempo");
-  var af = document.getElementById("foto");
-  hra1.innerHTML = "Agora são " + hra11 + " horas!";
-
-  if (hra11 >= 0 && hra11 <= 12) {
-    at.innerHTML = "Olá, muito bom dia!";
-    af.src = "./css/assets/data/dia.png";
-  }
-  if (hra11 > 12 && hra11 < 18) {
-    at.innerHTML = "Olá, muito boa tarde!";
-    af.src = "./css/assets/data/tarde.png";
-  }
-  if (hra11 >= 18 && hra11 < 23.59) {
-    at.innerHTML = "Olá, muito boa noite!";
-    af.src = "./css/assets/data/noite.png";
-  }
-}
-function timeInformed() {
-  var hr1 = document.getElementById("hr");
-  var hr2 = Number(hr1.value);
-  var tm = document.getElementById("tempo");
-  var ft = document.getElementById("foto");
-  var hras1 = document.getElementById("hra");
-
-  if (hr2 >= 0 && hr2 <= 12) {
-    tm.innerHTML = "Olá, muito bom dia!";
-    ft.src = "./css/assets/data/dia.png";
-    hras1.innerHTML = "Agora são " + hr2 + " horas!";
-  }
-
-  if (hr2 > 12 && hr2 < 18) {
-    tm.innerHTML = "Olá, muito boa tarde!";
-    ft.src = "./css/assets/data/tarde.png";
-    hras1.innerHTML = "Agora são " + hr2 + " horas!";
-  }
-
-  if (hr2 >= 18 && hr2 < 23.59) {
-    tm.innerHTML = "Olá, muito boa noite!";
-    ft.src = "./css/assets/data/noite.png";
-    hras1.innerHTML = "Agora são " + hr2 + " horas!";
-  }
-  if (hr2 > 24 || hr2 == null) {
-    hras1.innerHTML = "Esse horário não existe!";
-  }
-}
-
-//Start Modal Mechanics - Cursos --------------------------------
-//
-function mechanicModal() {
-  const modal = document.getElementById("mechanicModal");
-  modal.classList.add("open");
-
-  modal.addEventListener("click", (e) => {
-    if (e.target.id == "closed" || e.target.id == "windowMechanic") {
-      modal.classList.remove("open");
-    }
-  });
-}
-function edificationModal() {
-  const modal = document.getElementById("edificationModal");
-  modal.classList.add("open");
-
-  modal.addEventListener("click", (e) => {
-    if (e.target.id == "closed" || e.target.id == "windowEdification") {
-      modal.classList.remove("open");
-    }
-  });
-}
-function scrumFundamentsModal() {
-  const modal = document.getElementById("scrumFundamentsModal");
-  modal.classList.add("open");
-
-  modal.addEventListener("click", (e) => {
-    if (e.target.id == "closed" || e.target.id == "windowScrumFundaments") {
-      modal.classList.remove("open");
-    }
-  });
-}
-function leanModal() {
-  const modal = document.getElementById("leanModal");
-  modal.classList.add("open");
-
-  modal.addEventListener("click", (e) => {
-    if (e.target.id == "closed" || e.target.id == "windowLean") {
-      modal.classList.remove("open");
-    }
-  });
-}
-function programmingFundamentsModal() {
-  const modal = document.getElementById("programmingFundamentsModal");
-  modal.classList.add("open");
-
-  modal.addEventListener("click", (e) => {
-    if (e.target.id == "closed" || e.target.id == "windowProgrammingFundaments") {
-      modal.classList.remove("open");
-    }
-  });
-}
-
-//Start Pokedex ------------------------------------------------
-
-function convertPokemonToLi(pokemon) {
-  return `
-  <li>
-  <span class="number">#001</span>
-  <span class="name">${pokemon.name}</span>
-  <div class="status">
-    <ol>
-      <li>
-        Grass
-      </li>
-      <li>
-        Poison
-      </li>
-    </ol>
-    <img src=""https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.name}.png" alt="${pokemon.name}">
-  </div>
-</li>`;
-}
-const pokemonFigure = document.getElementById("pokemonFigure");
-
-pokeApi.getPokemons().then((pokemons) => {
-  pokemonFigure.innerHTML = pokemons.map(convertPokemonToLi).join("");
-});
