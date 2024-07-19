0dca03b426ed62a4306000178a99560dd1271766
IgoRenatoo <igor.renato.oliveira@gmail.com>
2024-07-19 12:13:21 -0300

    refactor: :recycle: Refatorado 'script.js' - Clean code :construction:

--- JS/script.js
+++ JS/script.js
@@ -6,9 +6,8 @@ if (getMode === "light") {
 }
 function toggleMode() {
   const html = document.body;
-  html.classList.toggle("light");
-
   let mode;
+  html.classList.toggle("light");
   if (html.classList.contains("light")) {
     mode = "light";
   } else {
@@ -16,32 +15,29 @@ function toggleMode() {
   }
   localStorage.setItem("PageTheme", JSON.stringify(mode));
 }
-
-function displayCourse(){
+function displayCourse() {
   //Armazena o número do curso selecionado.
   let course = document.getElementById("courseSelect").value;
   //Recebe como referência o local de destino da img src.
   let imgCourse = document.getElementById("displayCertification");
   //Objeto que contém todos os cursos especificados por números.
   const imagePaths = {
-    '1' : "./CSS/assets/certification/1 - Técnico Mecânica - SENAIES.jpg",
-    '2' : "./CSS/assets/certification/2 - Técnico Edificações - CEDTEC.jpg",
-    '3' : "./CSS/assets/certification/3 - Fundamentos Programação - Rocketseat.jpg",
-    '4' : "./CSS/assets/certification/4 - Banco de Dados - B7Web.jpg",
-    '5' : "./CSS/assets/certification/5 - Scrum Fundaments - ScrumStudy.jpg",
-    '6' : "./CSS/assets/certification/6 - Lean Manufacturing - RL Associados Front.jpg",
-    '7' : "./CSS/assets/certification/7 - Micro Certification - Welcome ServiceNow.jpg",
+    '1': "./CSS/assets/image/certification/1 - Técnico Mecânica - SENAIES.jpg",
+    '2': "./CSS/assets/image/certification/2 - Técnico Edificações - CEDTEC.jpg",
+    '3': "./CSS/assets/image/certification/3 - Fundamentos Programação - Rocketseat.jpg",
+    '4': "./CSS/assets/image/certification/4 - Banco de Dados - B7Web.jpg",
+    '5': "./CSS/assets/image/certification/5 - Scrum Fundaments - ScrumStudy.jpg",
+    '6': "./CSS/assets/image/certification/6 - Lean Manufacturing - RL Associados Front.jpg",
+    '7': "./CSS/assets/image/certification/7 - Micro Certification - Welcome ServiceNow.jpg",
   };
-  // Cria um elemento <img>.
+  // Estrutura da criação de um elemento <img>.
   let imgElement = document.createElement("img");
   //Adiciona class ao elemento criado.
-  imgElement.classList.add("imgCourse");
+  imgElement.classList.add("img-course");
   //Adiciona img ao elemento criado.
   imgElement.src = imagePaths[course];
-
   // Limpa o curso anterior selecionado.
   imgCourse.innerHTML = "";
-
   // Adiciona a imagem ao elemento criado.
   imgCourse.appendChild(imgElement);
 }
